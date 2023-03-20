3e74f17038b35002792a1563c310aa52b9a8d889
CHANG MIN PARK <98qkrckdals@naver.com>
2023-03-20 22:00:23 +0900

    commit : create Owners Entity Owners 엔티티 클라스입니다.

--- src/main/java/kr/co/pcmpetclinicstudy/persistence/entity/Owners.java
+++ src/main/java/kr/co/pcmpetclinicstudy/persistence/entity/Owners.java
@@ -1,4 +1,4 @@
-package kr.co.pcmpetclinicstudy.persistence.entity.entity;
+package kr.co.pcmpetclinicstudy.persistence.entity;
 
 import jakarta.persistence.*;
 import lombok.Builder;
@@ -13,7 +13,7 @@ public class Owners {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "owners_id")
+    @Column(name = "owners_id", length = 4) //Column(length -> 데이터베이스 안 열의 문자열의 길이 지정)
     private Long oId;
 
     @Column(name = "address")
@@ -22,13 +22,13 @@ public class Owners {
     @Column(name = "city")
     private String city;
 
-    @Column(name = "first_name")
+    @Column(name = "first_name", length = 30)
     private String firstName;
 
-    @Column(name = "last_name")
+    @Column(name = "last_name", length = 30)
     private String lastName;
 
-    @Column(name = "telephone", unique = true)
+    @Column(name = "telephone", unique = true, length = 30)
     private String telephone;
 
     @Builder
