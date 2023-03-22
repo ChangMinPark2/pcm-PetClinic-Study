0b5a0354ae42bb5fc5d1906823fc35ce89df3f0d
CHANG MIN PARK <98qkrckdals@naver.com>
2023-03-23 02:23:09 +0900

    commit 1 : create PetsTypes enum class

--- src/main/java/kr/co/pcmpetclinicstudy/persistence/entity/Pets.java
+++ src/main/java/kr/co/pcmpetclinicstudy/persistence/entity/Pets.java
@@ -2,6 +2,7 @@ package kr.co.pcmpetclinicstudy.persistence.entity;
 
 import jakarta.persistence.*;
 import kr.co.pcmpetclinicstudy.persistence.BaseEntity;
+import kr.co.pcmpetclinicstudy.service.model.PetsTypes;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,21 +19,26 @@ import java.time.LocalDate;
 public class Pets extends BaseEntity {
 
     @Column(name = "birth_date")
-    private LocalDate localDate;
+    private LocalDate birthDate;
 
     @Column(name = "name", length = 30)
     private String name;
+
+    @Column(name = "pets_types")
+    private PetsTypes petsTypes;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "owners_id")
     private Owners owners;
 
     @Builder
-    public Pets(LocalDate localDate,
+    public Pets(LocalDate birthDate,
                 String name,
-                Owners owners) {
-        this.localDate = localDate;
+                Owners owners,
+                PetsTypes petsTypes) {
+        this.birthDate = birthDate;
         this.name = name;
         this.owners = owners;
+        this.petsTypes = petsTypes;
     }
 }
