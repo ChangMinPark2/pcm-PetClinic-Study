02dcffc0e509db8f6374191551d2542107ae831b
CHANG MIN PARK <98qkrckdals@naver.com>
2023-04-02 03:18:12 +0900

    commit1 : createOwner(controller, service, repository)

--- src/main/java/kr/co/pcmpetclinicstudy/controller/OwnersController.java
+++ src/main/java/kr/co/pcmpetclinicstudy/controller/OwnersController.java
@@ -1,7 +1,11 @@
 package kr.co.pcmpetclinicstudy.controller;
 
+import kr.co.pcmpetclinicstudy.service.model.request.CreateOwnerDto;
 import kr.co.pcmpetclinicstudy.service.service.OwnersService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class OwnersController {
 
     private final OwnersService ownersService;
+
+    @PostMapping
+    public void createOwner(@RequestBody CreateOwnerDto createOwnerDto){
+        ownersService.createOwner(createOwnerDto);
+    }
 }
--- src/main/java/kr/co/pcmpetclinicstudy/service/service/OwnersService.java
+++ src/main/java/kr/co/pcmpetclinicstudy/service/service/OwnersService.java
@@ -1,6 +1,8 @@
 package kr.co.pcmpetclinicstudy.service.service;
 
+import kr.co.pcmpetclinicstudy.persistence.entity.Owners;
 import kr.co.pcmpetclinicstudy.persistence.repository.OwnersRepository;
+import kr.co.pcmpetclinicstudy.service.model.request.CreateOwnerDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,9 @@ import org.springframework.stereotype.Service;
 public class OwnersService {
 
     private final OwnersRepository ownersRepository;
+
+    public void createOwner(CreateOwnerDto createOwnerDto){
+        Owners owners = new Owners();
+        ownersRepository.save(owners);
+    }
 }
