50bebb1cf62afa375c2bdc4ce15b020ed27e3aa2
CHANG MIN PARK <98qkrckdals@naver.com>
2023-04-02 04:06:15 +0900

    commit 6 : UpdatePet(Controller, Service, Dto,)

--- src/main/java/kr/co/pcmpetclinicstudy/controller/PetsController.java
+++ src/main/java/kr/co/pcmpetclinicstudy/controller/PetsController.java
@@ -1,9 +1,11 @@
 package kr.co.pcmpetclinicstudy.controller;
 
 import kr.co.pcmpetclinicstudy.service.model.request.PetDto.CreatePetDto;
+import kr.co.pcmpetclinicstudy.service.model.request.PetDto.UpdatePetDto;
 import kr.co.pcmpetclinicstudy.service.service.PetsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,10 @@ public class PetsController {
     @PostMapping("/create")
     public void createPet(CreatePetDto createPetDto){
         petsService.createPet(createPetDto);
+    }
+
+    @PutMapping("/update")
+    public void updatePet(UpdatePetDto updatePetDto){
+        petsService.UpdatePet(updatePetDto);
     }
 }
---
+++ src/main/java/kr/co/pcmpetclinicstudy/service/model/request/PetDto/UpdatePetDto.java
@@ -0,0 +1,4 @@
+package kr.co.pcmpetclinicstudy.service.model.request.PetDto;
+
+public class UpdatePetDto {
+}
