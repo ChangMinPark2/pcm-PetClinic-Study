66667c980354db80a4dcc414be10584bf5858e03
CHANG MIN PARK <98qkrckdals@naver.com>
2023-04-11 16:17:14 +0900

    fix 16 : fix to vet

--- src/main/java/kr/co/pcmpetclinicstudy/service/service/VetService.java
+++ src/main/java/kr/co/pcmpetclinicstudy/service/service/VetService.java
@@ -1,7 +1,7 @@
 package kr.co.pcmpetclinicstudy.service.service;
 
-import kr.co.pcmpetclinicstudy.controller.infra.error.exception.VetNotFoundException;
-import kr.co.pcmpetclinicstudy.controller.infra.error.model.ErrorCodeType;
+import kr.co.pcmpetclinicstudy.infra.error.exception.VetNotFoundException;
+import kr.co.pcmpetclinicstudy.infra.error.model.ErrorCodeType;
 import kr.co.pcmpetclinicstudy.persistence.entity.Specialties;
 import kr.co.pcmpetclinicstudy.persistence.entity.Vet;
 import kr.co.pcmpetclinicstudy.persistence.entity.VetSpecialties;
@@ -78,35 +78,49 @@ public class VetService {
 
     private List<String> getSpecialtiesNameByVet(Vet vet){
 
-        return vet.getVetSpecialties().stream()
-                .map(VetSpecialties::getSpecialties)
-                .map(Specialties::getSpecialtiesName)
-                .collect(Collectors.toList());
+        return vet.getVetSpecialties().stream() //-> vetSpecialties를 스트림으로 변환,(각 분야 별로 순환 가능)
+                .map(VetSpecialties::getSpecialties)  //-> VetSpecialties를 Specialties로 변환
+                .map(Specialties::getSpecialtiesName) //-> Specialties의 이름만 추출.
+                .collect(Collectors.toList()); //-> collect를 통해 리스트로 변환.
     }
 
+    /**
+     * Specialties 이름 리스트를 이용해, 해당 이름으로 검색한 전문 분야 객체 리스트를 가져옴
+     * 새로운 이름이 있으면 -> 새로운 전문 분야 객체를 생성하여 반환함
+     * */
     private List<Specialties> getOrCreateSpecialtiesByNames(List<String> names){
 
         List<Specialties> specialties = specialtiesRepository.findAllByName(names);
 
-        final Set<String> existNames = specialties.stream()
-                .map(Specialties::getSpecialtiesName)
-                .collect(Collectors.toSet());
+        final Set<String> existNames = specialties.stream()//speciallties -> 스트림으로 변환
+                .map(Specialties::getSpecialtiesName) //-> 전문 분야 객체의 이름을 추출
+                .collect(Collectors.toSet()); //중복되지 않은 전문 분야 이름의 set 컬렉션 생성
 
         final List<Specialties> createSpecialties = names.stream()
-                .filter(name -> !existNames.contains(name))
-                .map(specialtiesMapper::toSpecialtiesEntity)
-                .collect(Collectors.toList());
+                .filter(name -> !existNames.contains(name))// existNames Set 컬렉션에 포함되지 않는 이름만 추출
+                .map(specialtiesMapper::toSpecialtiesEntity) // 해당 이름으로 새로운 전문분야 객체 생성
+                .collect(Collectors.toList());  // 새로운 전문분야 객체 -> createSpecialties 리스트에 저장
 
-        specialties.addAll(createSpecialties);
+        specialties.addAll(createSpecialties); // 리스트에 새로 생성한 전문 분야 객체 리스트를 추가.
 
         return specialties;
     }
 
+    /**
+     * 학위이름 리스트와, Vet 객체를 이용해 해당 이름으로 검색한 전문 분야 객체를 가져온다.
+     * 새로운 전문 분야 객체를 생성한 후 Vet 객체와 연결한 VetSpecialties 객체 리스트를 반환한다.
+     * */
     private List<VetSpecialties> getOrCreateVetSpecialties(List<String> names,
                                                            Vet vet){
 
+        //입력받은 이름 리스트를 이용하여, 검색한 번문 분야 객체 리스트를 가져온다.
+        //가져온 전문 분야 객체 리스트를 specialties 리스트에 저장한다.
         final List<Specialties> specialty = getOrCreateSpecialtiesByNames(names);
 
+        //입력받은 이름 리스트를 이용하여, 검색한 번문 분야 객체 리스트를 가져온다.
+        //가져온 전문 분야 객체 리스트를 specialties 리스트에 저장한다.
+        //map -> 각 전문 분야 객체에 대응되는 VetSpecialties를 생성
+        //Mapper -> 해당 전문 분야 객체와 Vet 객체를 이용하여 새로운 VetSpecialties객체를 생성
         return specialty.stream()
                 .map(specialties -> vetSpecialtiesMapper.toVetSpecialtiesEntity(specialties, vet))
                 .collect(Collectors.toList());
